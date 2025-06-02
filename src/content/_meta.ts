e15655e5df072b5c92c6aa89d1bbb573b3452598
Benceszalaiii <120375165+Benceszalaiii@users.noreply.github.com>
2025-06-02 11:14:30 +0200

    Update _meta.ts

--- src/content/_meta.ts
+++ src/content/_meta.ts
@@ -11,6 +11,11 @@ const meta: MetaRecord = {
     },
     title: "Ágazati felkészülés | Khrone",
   },
+donate: {
+type: 'page',
+title: 'Show support',
+href: 'https://checkout.revolut.com/pay/9b2b4f6e-4f70-46a1-9a2c-426a699f19ab'
+},
   halozat: "Hálózat",
   py: "Python",
   web: "Web",
